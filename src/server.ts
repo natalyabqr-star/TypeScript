d1cd5f88381fe65ef14fbe1559981ebb59379724
natalyabqr-star <natalyabqr@gmail.com>
2026-09-18 11:15:42 -0300

    Instalando primeiras biblioteca

---
+++ src/server.ts
@@ -0,0 +1,14 @@
+interface User {
+    name: string
+    age: number
+}
+function saveUserToDatebase(user: User)  {
+    // salvar no banco de dados
+
+
+console.log(user)
+}
+saveUserToDatebase({
+    name: 'natálya',
+    age: 24
+})
